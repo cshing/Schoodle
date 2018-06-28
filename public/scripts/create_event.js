cd8a225735542ee9a340dbdd715564e3943bfd7d
lauratan <laura.tan108@gmail.com>
2018-06-28 22:42:46 +0000

    added datepair npm for create event form, added datepair links to _head

--- public/scripts/create_event.js
+++ public/scripts/create_event.js
@@ -1,11 +1,15 @@
-$(() => {
-    $.ajax({
-      method: "GET",
-      url: "/api/events"
-    }).done((users) => {
-      for(user of users) {
-        $("<div>").text(user.email).appendTo($("body"));
-      }
-    });;
+$(document).ready(function(){
+  $('#datepair .time').timepicker({
+  'showDuration': true,
+  'timeFormat': 'g:ia'
   });
-  
+
+  $('#datepair .date').datepicker({
+    'format': 'm/d/yyyy',
+    'autoclose': true
+  });
+
+  // initialize datepair
+  var basicExampleEl = document.getElementById('datepair');
+  var datepair = new Datepair(basicExampleEl);
+});
