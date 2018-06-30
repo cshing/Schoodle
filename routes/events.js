9e323f754b5f54ea3a9df335976de637f4a99f33
lauratan <laura.tan108@gmail.com>
2018-06-30 20:50:34 +0000

    added checkArray function for timeslot(s)

--- routes/events.js
+++ routes/events.js
@@ -8,17 +8,25 @@ module.exports = (knex) => {
   router.post("/", (req, res) => {
     let formData = req.body;
     let timeSlots = [];
-    
-    // compileStartAndEndDates
-    formData.start_date.map((date, index) => {
-      let data = {
-        start_date: formData.start_date[index],
-        start_time: formData.start_time[index],
-        end_date: formData.end_date[index],
-        end_time: formData.end_time[index]
+
+    //check if req.body.start_date is an array
+    function checkArray(){
+      if (Array.isArray(formData.start_date)){
+
+        // compileStartAndEndDates
+        formData.start_date.map((date, index) => {
+          let data = {
+            start_date: formData.start_date[index],
+            start_time: formData.start_time[index],
+            end_date: formData.end_date[index],
+            end_time: formData.end_time[index]
+          }
+          timeSlots.push(data);
+        })
+        return;
       }
-      timeSlots.push(data);
-    })
+      return false;
+    }
 
     knex('events')
     .insert({
@@ -29,29 +37,43 @@ module.exports = (knex) => {
     })
     .returning('id')
     .then(function(response){
-      timeSlots.map(item => {
-        knex('timeslots')
-        .insert({
-          start_date: item.start_date,
-          start_time: item.start_time,
-          end_time: item.end_time,
-          end_date: item.end_date,
-          event_id: response[0]
+      // console.log(formData.start_date);
+      if (checkArray){
+        timeSlots.map(item => {
+          knex('timeslots')
+          .insert({
+            start_date: item.start_date,
+            start_time: item.start_time,
+            end_time: item.end_time,
+            end_date: item.end_date,
+            event_id: response[0]
+          })
         })
-        .then(() => {
-          knex
-          .select('url')
-          .from('events')
-          .then((rows) => {
-            //console.log(rows.slice(-1)[0].url);
-            // res.json(rows)
-            return res.redirect(`/e/${rows.slice(-1)[0].url}`);
-        })
-      })
-        .catch(err => res.send("Error in events.js in routes:", err))
+      } 
+      knex('timeslots')
+      .insert({
+        start_date: formData.start_date,
+        start_time: formData.start_time,
+        end_time: formData.end_time,
+        end_date: formData.end_date,
+        event_id: response[0]
       })
     })
+    .then(() => {
+      knex
+      .select('url')
+      .from('events')
+      .then((rows) => {
+        //console.log(rows.slice(-1)[0].url);
+        // console.log(rows);
+        // res.json(rows)
+        // console.log(res.status(304));
+        return res.redirect(`/e/${rows.slice(-1)[0].url}`);
+      })
+      .catch(err => res.send("Error in events.js in routes:", err))
+    })
   });
+  
 
   //Generate 10 random characters
   function generateRandUrl() {
